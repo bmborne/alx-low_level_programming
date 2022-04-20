1269082761a2ef158e8b79ed965b2dd8adae2d2a
bmborne <boniphacemkindi@gmail.com>
2022-04-20 11:15:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
     /*
      * main - Entry point
@@ -12,8 +11,8 @@ int
 main()
 
 {
-	char str1[] = '"Programming is like building a multilingual puzzle';
-	puts(str1);
+	char str[] = '\"Programming is like building a multilingual puzzle';
+	puts(str);
 
 	return (0);
 }
