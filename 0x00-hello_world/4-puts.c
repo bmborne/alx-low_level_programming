873706711ceae7cfb75c7ca187f28276eab0c3db
bmborne <boniphacemkindi@gmail.com>
2022-04-20 11:19:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@ int
 main()
 
 {
-	char str[] = '\"Programming is like building a multilingual puzzle';
+	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 
 	return (0);
