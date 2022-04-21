732c0fe630d0a2f4c45bfecfd38f7e1eb659cd5d
bmborne <boniphacemkindi@gmail.com>
2022-04-21 01:17:15 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,18 +12,17 @@ main(void)
 	/* Space after declaration */
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit1 + 10; digit2 < 1; digit2++)
-
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-
 			if (digit1 == 8 && digit2 == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
