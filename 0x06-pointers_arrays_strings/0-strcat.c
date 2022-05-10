a1470bdd83fbee63b0264c4b4ac06cc9fe8fe843
bmborne <boniphacemkindi@gmail.com>
2022-05-10 06:17:09 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,20 @@ include "main.h"
  *
  * Return: Always 0.
  */
-int main(void)
-{
-char s1[98] = "Hello ";
-char s2[] = "World!\n";
-char *ptr;
 
-printf("%s\n", s1);
-printf("%s", s2);
-ptr = _strcat(s1, s2);
-printf("%s", s1);
-printf("%s", s2);
-printf("%s", ptr);
-return (0);
+char char *_strcat(char *dest, char *src)
+{
+int count = 0;
+int count2 = 0;
+
+while (*(dest + count) != '\0')
+{
+count++;
+}
+
+while (*(src + count2) != '\0')
+{
+count2++;
+}
+return (dest + src);
 }
