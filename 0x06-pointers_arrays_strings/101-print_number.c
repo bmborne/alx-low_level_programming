d8b8f18ba9831f31173ce2bd881fb27e7cfef51c
bmborne <boniphacemkindi@gmail.com>
2022-05-10 07:35:16 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ unsigned int m, d, count;
 
 if (n < 0)
 {
-_putchar(45);
+putchar(45);
 m = n * -1;
 }
 else
@@ -30,6 +30,6 @@ count *= 10;
 
 for (; count >= 1; count /= 10)
 {
-_putchar(((m / count) % 10) + 48);
+putchar(((m / count) % 10) + 48);
 }
 }
