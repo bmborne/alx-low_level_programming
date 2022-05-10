ee8178b132440578c90865feb8d5ec5d388bc95d
bmborne <boniphacemkindi@gmail.com>
2022-05-10 06:32:04 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,9 +16,13 @@ while (*(dest + count) != '\0')
 count++;
 }
 
-while (*(src + count2) != '\0')
+while (count2 >= 0)
 {
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+break;
+count++;
 count2++;
 }
-return (dest + src);
+return (dest);
 }
