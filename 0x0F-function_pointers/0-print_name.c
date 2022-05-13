bcd777455cde8657953d28a0210f6fb12ad301eb
bmborne <boniphacemkindi@gmail.com>
2022-05-13 11:47:01 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,6 @@
+#include "main.h"
 #include <stdio.h>
-/**
- * print_name - Function to call a function
- *
- * @name: Parameter 1
- * @f: parameter function
- */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	f(name);
 }
