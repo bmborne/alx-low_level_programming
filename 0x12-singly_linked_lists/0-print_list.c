1f21ad9acd1f10842d6aa2d5e18913f51836bbf8
bmborne <boniphacemkindi@gmail.commit>
2022-05-26 09:38:27 -0700

    linkedlists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,18 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
-while (h)
+site_t i;
+for (i = 0; h != NULL; i++)
 {
 if (h->str == NULL)
-printf("[%d] (%s)\n", 0, "nil");
+{
+printf("[0] (nil)\n");
+}
 else
 {
-printf("[%d] (%s)\n", h->len, h->str);
+printf("[%u] %s\n", h->len, h->str);
 }
 h = h->next;
-count++;
 }
-return (count);
+return (i);
 }
