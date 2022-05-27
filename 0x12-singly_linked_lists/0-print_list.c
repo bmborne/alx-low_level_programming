27ffc42210bad5b9ad3e5f6eb6ebc09bf7124940
bmborne <boniphacemkindi@gmail.commit>
2022-05-27 02:27:40 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,27 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * print_list - is a function that prints all elements of a list_t list
- * @h: is the string input
- * Return: number of nodes
+ * print_list - function that prints all the elements of a list_t list.
+ *
+ * @h: Parameter with the list
+ *
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-const list_t *tmp;
-unsigned int i;
+	size_t i;
 
-tmp = h;
-for (i = 0; tmp; i++)
-{
-printf("[%u] %s\n", tmp->len, tmp->str);
-tmp = tmp->next;
-}
-return (i);
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+
+	return (i);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,10 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
-size_t i;
-for (i = 0; h != NULL; i++)
-{
-h = h->next;
-}
-return (i);
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+
+	return (i);
 }
