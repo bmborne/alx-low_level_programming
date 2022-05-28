083e3d17bc04d9678841a6ab1ca20b459a7883f4
bmborne <boniphacemkindi@gmail.commit>
2022-05-28 02:35:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - prints to string
+ * main - Entryp Point of the program
  *
- * Description: Prints "and that piece of art is useful..." without puts
- *
- * Return: 1
+ * Return: The Progrem returns 1 (stderr)
  */
+
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-long l = 59;
-long fd = 1;
-long syscall = 1;
-long ret = 0;
-__asm__ ("syscall"
-: "=a" (ret)
-: "a" (syscall),
-"D" (fd),
-"S" (s),
-"d" (l));
-return (1);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", str);
+	return (1);
 }
